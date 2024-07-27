2740cb3eea9a9e6dfe11684a47edfb312be33fd2
lsara011 <saravia2019@hotmail.com>
2024-07-27 16:19:39 -0400

    Added feautre to seeAllViewController

---
+++ ProductSystem/ProductTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProductTableViewCell.swift
+//  ProductSystem
+//
+//  Created by Luis Saravia on 7/27/24.
+//
+
+import UIKit
+
+class ProductTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
